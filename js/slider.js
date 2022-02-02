24afe50feeb1f67e840ed60b504735c5b862314d
NoruziSamaneh <noruzi.samaneh793@gmail.com>
2022-02-02 16:52:23 +0330

    add our barbers section. i use from slider in this section

--- js/slider.js
+++ js/slider.js
@@ -13,3 +13,18 @@ $(document).ready(() => {
       nextArrow: '<div class="container-md"><i class="fas fa-angle-right arrow" aria-hidden="true"></i></div>'
   });
 });
+
+$(document).ready(() => {
+  $("#barbers-slider").slick({
+      arrows: true,
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: "linear",
+      rtl: true,
+      autoplay: false,
+      prevArrow: '<div class="container-md"><i class="fas fa-angle-left barbers-arrow left-barbers-arrow" aria-hidden="true"></i></div>',
+      nextArrow: '<div class="container-md"><i class="fas fa-angle-right barbers-arrow right-barbers-arrow" aria-hidden="true"></i></div>'
+  });
+});
